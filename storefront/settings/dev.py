a487f73f73c3eadb9eac2b99cca8aab100fcc207
Ecodes-REST <shammahcodes@gmail.com>
2024-03-08 20:36:53 +0300

    Initial commit 007

--- storefront/settings/dev.py
+++ storefront/settings/dev.py
@@ -11,15 +11,14 @@ SECRET_KEY = 'django-insecure-hs6j037urx6iav+7#10%-vu4l4f5@@-1_zo)oft4g7$vf2$jmp
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'storefront3',
-        'HOST': 'localhost',
-        'PORT':'5432',
-        'USER': 'E-Mollz',
-        'PASSWORD': 'Jr. Web Developer'
+        'NAME': 'railway',
+        'HOST': 'viaduct.proxy.rlwy.net',
+        'PORT':'57126',
+        'USER': 'postgres',
+        'PASSWORD': 'JBSTofsnkyJgGMgOURAkQsdXgdXkgoQj'
     }
 }
 
