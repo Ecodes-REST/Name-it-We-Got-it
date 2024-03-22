802061b134ca123184aae6f75187783643f3c9ec
Ecodes-REST <shammahcodes@gmail.com>
2024-03-22 23:40:26 +0300

    Intial commit 0011

--- storefront/settings/dev.py
+++ storefront/settings/dev.py
@@ -7,7 +7,7 @@ if DEBUG:
     MIDDLEWARE += ['silk.middleware.SilkyMiddleware']
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-hs6j037urx6iav+7#10%-vu4l4f5@@-1_zo)oft4g7$vf2$jmp')
+SECRET_KEY = 'django-insecure-hs6j037urx6iav+7#10%-vu4l4f5@@-1_zo)oft4g7$vf2$jmp'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
